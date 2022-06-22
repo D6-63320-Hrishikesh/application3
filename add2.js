a9615b59e64f5e5d61e5697ec395893717297036
hrishi <hrishikesh.auti@gmail.com>
2022-06-22 20:10:50 +0530

    added b1

--- add2.js
+++ add2.js
@@ -4,3 +4,9 @@ function add(a, b) {
 function mul(a, b) {}
 function div(a, b) {}
 function sub(a, b) {}
+function mul(a, b) {}
+function div(a, b) {}
+function sub(a, b) {}
+function mul(a, b) {}
+function div(a, b) {}
+function sub(a, b) {}
